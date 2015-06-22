b641435dd683bce713419970354d0f8f45874aba
Naren <ntallapr@users.noreply.github.com>
2015-06-22 11:55:46 -0400

    Committinglocal MATLAB files

---
+++ loadImg.m
@@ -0,0 +1,24 @@
+function image_5d = loadImg
+% N. Tallapragada -- June 2015
+% Code to convert a stack of confocal images of an organoid into a 5D array
+
+%% Load data and function arguments
+data = bfopen;
+imgData = data{1,1};
+
+%% Calculate number of slices in stack from number of images and number of channels
+numImgs = length(imgData);
+% Number of channels is the last character in the string imgData{1,2} (if 2-digit, it's everything after the /)
+numChan = str2num(imgData{1,2}(end));
+numSlices = numImgs/numChan;
+
+for chanNum = 1:numChan
+    %% Build 5D array
+    z_slice = 1;
+    for img = chanNum:numChan:numImgs
+        image_5d(:,:,z_slice,chanNum,1) = double(imgData{img,1});
+        z_slice = z_slice + 1;
+    end
+end
+
+end
